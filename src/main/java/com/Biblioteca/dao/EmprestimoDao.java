36bd1d9eafe3a85632feaff4e55810709025d28a
Alisson Ricarte <alissonricarte7575@gmail.com>
2025-11-27 13:50:27 -0300

    feat: Finalizando a função EmprestimoDao

--- src/main/java/com/Biblioteca/dao/EmprestimoDao.java
+++ src/main/java/com/Biblioteca/dao/EmprestimoDao.java
@@ -14,6 +14,7 @@ import com.Biblioteca.util.DatabaseConnetion;
 
 public class EmprestimoDao {
     
+    //função aux 
     private Emprestimo criarEmprestimoFromResultSet(ResultSet resultSet) throws SQLException{
         java.sql.Date dataDevolucaoReal = resultSet.getDate("data_devolucao_real");
         LocalDate dataReal = (dataDevolucaoReal != null) ? dataDevolucaoReal.toLocalDate() : null;
@@ -27,6 +28,8 @@ public class EmprestimoDao {
             dataReal, 
             resultSet.getString("status"));
     }
+
+    //fazer emprestimo 
     public void realizarEmpretimo(Emprestimo emprestimos) throws SQLException{
         String sql = "INSERT INTO emprestimo (livro_id, usuario_id, data_devolucao_prevista) VALUES (?, ?, ?)";
 
@@ -39,6 +42,8 @@ public class EmprestimoDao {
                 stmt.executeUpdate();
             }
     }
+
+    //buscar emprestimo por id
     public Emprestimo buscarPorId(int id) throws SQLException{
         String sql = "SELECT * FROM emprestimo WHERE id = ?";
         Emprestimo emprestimo = null;
@@ -55,6 +60,8 @@ public class EmprestimoDao {
             }
         return emprestimo;    
     }
+
+    //listar emprestimo ativo 
     public List<Emprestimo> listarEmpretimoAtivo() throws SQLException{
         List<Emprestimo> emprestimos = new ArrayList<>();
         String sql = "SELECT * FROM emprestimo WHERE status = 'ATIVO' ORDER BY data_emprestimo DESC";
@@ -69,5 +76,169 @@ public class EmprestimoDao {
                 }
             }
         return emprestimos;    
+    }
+
+    //buscar emprestio por usuario_id
+    public List<Emprestimo> listarPorUsuario (int usuario_id) throws SQLException{
+        List<Emprestimo> emprestimos = new ArrayList<>();
+        String sql = "SELECT * FROM emprestimo WRERE usuario_id = ? ORDER BY data_emprestimo DESC";
+
+        try(Connection conn = DatabaseConnetion.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+                stmt.setInt(1, usuario_id);
+                ResultSet resultSet = stmt.executeQuery();
+
+                while(resultSet.next()){
+                    Emprestimo emprestimo = criarEmprestimoFromResultSet(resultSet);
+                    emprestimos.add(emprestimo);
+                }
+            }
+        return emprestimos;    
+    }
+
+    // verificar se livro está emprestado
+    public boolean livroEmprestado(int livro_id) throws SQLException{
+        String sql = "SELECT 1 FROM emprestimo WHERE livro_id = ? AND status = 'ATIVO'";
+
+        try(Connection conn = DatabaseConnetion.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+                stmt.setInt(1, livro_id);
+                ResultSet resultSet = stmt.executeQuery();
+                
+                return resultSet.next();
+            }
+    }
+
+    //Verificar empréstimos ativos de um usuário
+    public List<Emprestimo> listarAtivosPorUsuario(int usuario_id) throws SQLException{
+        List<Emprestimo> emprestimos = new ArrayList<>();
+        String sql = "SELECT * FROM emprestimo WHERE usuario_id = ? AND  status = 'ATIVO' ORDER BY data_emprestimo DESC";
+
+        try(Connection conn = DatabaseConnetion.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+                stmt.setInt(1, usuario_id);
+                ResultSet resultSet = stmt.executeQuery();
+                while (resultSet.next()) {
+                    Emprestimo emprestimo = criarEmprestimoFromResultSet(resultSet);
+                    emprestimos.add(emprestimo);
+                }
+            }
+        return emprestimos;    
+    }
+
+    //registrar devolução
+    public void resgistarDevolucao(int id) throws SQLException{
+        String sql = "UPDATE emprestimo SET data_devolucao_real = CURRENT_DATE , status = 'FINALIZADO' WHERE id = ?";
+
+        try(Connection conn = DatabaseConnetion.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+                stmt.setInt(1, id);
+
+                int linhasAfetadas = stmt.executeUpdate();
+
+                if(linhasAfetadas == 0){
+                    throw new SQLException("Falha ao registrar devolução - empréstimo não encontrado");
+                }
+            }
+    }
+
+
+    //registrar devolução com data específica
+    public void resgistarDevolucao(int id, LocalDate data_devolucao) throws SQLException{
+        String sql = "UPDATE emprestimo SET data_devolucao_real = ? , status = 'FINALIZADO' WHERE id = ?";
+
+        try(Connection conn = DatabaseConnetion.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+                stmt.setDate(1, Date.valueOf(data_devolucao));
+                stmt.setInt(2, id);
+
+                int linhasAfetadas = stmt.executeUpdate();
+                if (linhasAfetadas == 0) {
+                    throw new SQLException("Falha ao registrar devolução - empréstimo não encontrado"); 
+                }
+            }
+    }
+
+    //listar empréstimos atrasados
+    public List<Emprestimo> listarEmprestimoAtrasado () throws SQLException{
+        List<Emprestimo>  emprestimos = new ArrayList<>();
+        String sql = "SELECT * FROM emprestimo WHERE status = 'ATIVO' AND data_devolucao_prevista < CURRENT_DATE ORDER BY data_devolucao_prevista ASC";
+
+        try(Connection conn = DatabaseConnetion.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet resultSet = stmt.executeQuery()){
+
+                while(resultSet.next()){
+                    Emprestimo emprestimo = criarEmprestimoFromResultSet(resultSet);
+                    emprestimos.add(emprestimo);
+                }
+            }
+        return emprestimos;    
+    }
+
+    //histórico completo de empréstimos
+    public List<Emprestimo> listarHistorico () throws SQLException{
+        List<Emprestimo>  emprestimos = new ArrayList<>();
+        String slq = "SELECT * FROM emprestimo ORDER BY data_emprestimo DESC";
+
+        try(Connection conn = DatabaseConnetion.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(slq);
+            ResultSet resultSet = stmt.executeQuery()){
+
+                while(resultSet.next()){
+                    Emprestimo emprestimo = criarEmprestimoFromResultSet(resultSet);
+                    emprestimos.add(emprestimo);
+                }
+            }
+        return emprestimos;
+        
+    }
+
+    //contar empréstimos ativos
+    public int contarEmprestimoAtivo() throws SQLException{
+        String sql = "SELECT COUNT(*) as total FROM emprestimo WHERE status = 'ATIVO'";
+
+        try(Connection conn = DatabaseConnetion.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet resultSet = stmt.executeQuery()){
+
+                if(resultSet.next()){
+                    return resultSet.getInt("total");
+                }
+            }
+        return 0;    
+    }
+
+    //contar empréstimos atrasados
+    public int contarEmprestimoAtrasdo () throws SQLException{
+        String sql = "SELECT COUNT(*) as total FROM emprestimo WHERE status = 'ATIVO' AND data_devolucao_prevista < CURRENT_DATE";
+
+        try(Connection conn = DatabaseConnetion.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet resultSet = stmt.executeQuery()){
+
+                if(resultSet.next()){
+                    return resultSet.getInt("total");
+                }
+            }
+        return 0;    
+    }
+
+    //verificar se empréstimo existe
+    public boolean verificarSeEmprestimoExiste (int id) throws SQLException{
+        String sql = "SELECT 1 FROM emprestimo WHERE id = ?";
+
+        try(Connection conn = DatabaseConnetion.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+                stmt.setInt(1, id);
+                ResultSet resultSet = stmt.executeQuery();
+
+                return resultSet.next();
+            }
     }
 }
